9ce25de75bde8c995f31cb7c059dd141e38301db
Tung Nguyen <7319tnguyen040@gmail.com>
2023-03-06 22:38:48 +0000

    Completed lab

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -30,6 +30,8 @@ public class Lab {
      * @return a string representation of all values of nums, with the values separated by spaces.
      */
     public String arrayToString(int[] nums){
-        return "";
+        String retVal = "";
+        for (int i = 0; i < nums.length; i++) retVal += nums[i] + " ";
+        return retVal;
     }
 }
